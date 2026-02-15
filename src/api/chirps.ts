45606499a470307986d2f02034cc9894a3a8a361
malwani <98039304+malwani9@users.noreply.github.com>
2026-02-15 22:27:21 +0300

    refactor handlerChirpCreate

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -10,27 +10,13 @@ type Chirp = {
 
 export async function handlerChirpsCreate(req: Request, res: Response) {
 
-    const profane = ["kerfuffle", "sharbert", "fornax"];
     const params: Chirp = req.body;
 
     if (!params.body || !params.userId) {
         throw new BadRequestError("Missing required fields");
     }
 
-    if  (params.body.length > 140) {
-        throw new BadRequestError("Chirp is too long. Max length is 140");
-    }
-    
-
-    const request_words = params.body.split(" ");
-    
-    for (let i = 0; i < request_words.length; i++) {
-        if (profane.includes(request_words[i].toLocaleLowerCase())) {
-            request_words[i] = "****";
-        }
-    }
-
-    const cleaned = request_words.join(" "); 
+    const cleaned = validateChirp(params.body);
 
     const result = await createChirp({ body: cleaned, userId: params.userId});
     if (!result) {
@@ -45,4 +31,27 @@ export async function handlerChirpsCreate(req: Request, res: Response) {
     
     JSONResponse(res, 201, chirp);  
     
+}
+
+function validateChirp(body: string) {
+    const maxChirpLenght = 140;
+    if  (body.length > maxChirpLenght) {
+        throw new BadRequestError("Chirp is too long. Max length is 140");
+    }
+
+    const profanes = ["kerfuffle", "sharbert", "fornax"];
+    return cleanChirpBody(body, profanes)
+}
+
+function cleanChirpBody(body: string, profanes: string[]) {
+    const words = body.split(" ");
+    
+    for (let i = 0; i < words.length; i++) {
+        if (profanes.includes(words[i].toLocaleLowerCase())) {
+            words[i] = "****";
+        }
+    }
+
+    const cleaned = words.join(" ");
+    return cleaned;
 }
