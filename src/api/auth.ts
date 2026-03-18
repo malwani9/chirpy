c044a9b29afbc0d20aea8db4c0f194fe4769fb8b
malwani <98039304+malwani9@users.noreply.github.com>
2026-03-18 06:02:00 +0300

    Add password hashing, and jwt creation and validation

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,10 @@
 import type { Request, Response} from "express";
 import { getUserByEmail } from "../db/queries/users.js";
-import { BadRequestError, UnauthorizedError } from "./errorMiddleware.js";
+import { BadRequestError, NotFoundError, UnauthorizedError } from "./errorMiddleware.js";
 import { checkPasswordHash } from "../auth.js";
 import { JSONResponse } from "./json.js";
 import { UserResponse } from "../db/schema.js";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 type parameters = {
     password: string;
@@ -22,7 +23,7 @@ export async function handlerLogin(req: Request, res: Response) {
     if (!user) {
         throw new UnauthorizedError("incorrect email or password");
     }
-    console.log("after eamil condition");
+
     const match = await checkPasswordHash(user.hashed_password, params.password);
     if (!match) {
         throw new UnauthorizedError("incorrect email or password");
@@ -34,4 +35,32 @@ export async function handlerLogin(req: Request, res: Response) {
         createdAt: user.createdAt,
         updatedAt: user.updatedAt,
     } satisfies UserResponse);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+    const payload: Payload = {
+        iss: "chirpy",
+        sub: userID,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + expiresIn,
+    }
+
+    const token = jwt.sign(payload, secret);
+
+    return token;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    try {
+        const decoded = jwt.verify(tokenString, secret) as jwt.JwtPayload;
+        if (!decoded.sub) {
+            throw new NotFoundError("Token payload is missing subject");
+        }
+        return decoded.sub;
+    } catch (err) {
+        throw new UnauthorizedError("Unathorized access");
+    }
+    
 }
